b286a513b1311b550a72be5043cdc06fa865e63c
Jason Lagao <jasonlagao@gmail.com>
2018-01-19 00:35:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {
   artistName: 'songTitle'
 }
 
+<<<<<<< HEAD
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
@@ -11,3 +12,6 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
+=======
+function 
+>>>>>>> 79a95a6bbd9839e17febf8ec74d2e449fd45354d
